67db8550d8041f4c387a3770b61f5a02b7727d4d
juchan204 <schultz19@naver.com>
2024-10-30 18:05:39 +0900

    FEAT : member 관련 DTO 구현 #4

---
+++ src/main/kotlin/com/example/echo/domain/member/dto/request/MemberLoginRequest.kt
@@ -0,0 +1,14 @@
+package com.example.echo.domain.member.dto.request
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+
+data class MemberLoginRequest(
+    @Schema(description = "사용자 ID", example = "user123")
+    @field:NotBlank(message = "아이디는 필수 항목입니다.")
+    val userId: String,
+
+    @Schema(description = "사용자 비밀번호", example = "1111")
+    @field:NotBlank(message = "비밀번호는 필수 항목입니다.")
+    val password: String
+)
