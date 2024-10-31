a24970ff71c1992b5bc30278b784a2e027259d68
username0w <grandcias123@gmail.com>
2024-10-31 15:33:02 +0900

    feat: Inquiry 기본값 설정

--- src/main/kotlin/com/example/echo/domain/inquiry/entity/Inquiry.kt
+++ src/main/kotlin/com/example/echo/domain/inquiry/entity/Inquiry.kt
@@ -9,12 +9,12 @@ import java.time.LocalDateTime
 @Entity
 @Table(name = "tbl_inquiry")
 @EntityListeners(AuditingEntityListener::class)
-data class Inquiry (
+class Inquiry (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "inquiry_id", nullable = false, unique = true)
-    val inquiryId: Long,
+    val inquiryId: Long? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
@@ -25,21 +25,21 @@ data class Inquiry (
     var inquiryCategory: InquiryCategory,
 
     @Column(name = "inquiry_title", nullable = false)
-    var inquiryTitle: String,
+    var inquiryTitle: String = "",
 
     @Column(name = "inquiry_content", length = 2000, nullable = false)
     var inquiryContent: String = "",
 
     @CreatedDate
     @Column(name = "created_date", nullable = false, updatable = false)
-    val createdDate: LocalDateTime,
+    val createdDate: LocalDateTime = LocalDateTime.now(),
 
     @Column(name = "reply_content", length = 2000) // 문의 등록 시 관리자 답변 null
     var replyContent: String? = null,
 
     @Enumerated(EnumType.STRING)
     @Column(name = "inquiry_status", nullable = false)
-    var inquiryStatus: InquiryStatus,
+    var inquiryStatus: InquiryStatus = InquiryStatus.PENDING,
 
     @Column(name = "replied_date")
     var repliedDate: LocalDateTime? = null,
