cd5784ecc3e2aca3ed1e2134e01562f39b8385a8
jooinjoo <jooinjoo0@gmail.com>
2024-11-01 10:19:43 +0900

    test: 청원 크롤링, 청원 요약 테스트 #16

--- src/main/kotlin/com/example/echo/domain/member/dto/response/MemberResponse.kt
+++ src/main/kotlin/com/example/echo/domain/member/dto/response/MemberResponse.kt
@@ -1,6 +1,5 @@
 package com.example.echo.domain.member.dto.response
 
-import com.example.echo.domain.member.entity.Member
 import com.example.echo.domain.member.entity.Role
 import io.swagger.v3.oas.annotations.media.Schema
 
@@ -10,20 +9,20 @@ data class MemberResponse (
     val memberId: Long? = null,
 
     @field:Schema(description = "사용자 ID", example = "user1")
-    val userId: String,
+    val userId: String = "",
 
     @field:Schema(description = "회원 이름", example = "홍길동")
-    val name: String,
+    val name: String = "",
 
     @field:Schema(description = "회원 이메일", example = "user1@example.com")
-    val email: String,
+    val email: String = "",
 
     @field:Schema(description = "회원 전화번호", example = "010-1234-5678")
-    val phone: String,
+    val phone: String = "",
 
     @field:Schema(description = "회원 프로필 이미지 URL", example = "/images/default-avatar.png")
     val avatarImage: String? = null,
 
     @field:Schema(description = "회원 역할", example = "USER")
-    val role: Role
+    val role: Role = Role.USER
 )
