2a0863c7292182ec16ec367ae79dd1eee2f9e66a
username0w <grandcias123@gmail.com>
2024-11-01 12:06:42 +0900

    feat: @Query join fetch 사용

--- src/main/kotlin/com/example/echo/domain/inquiry/repository/InquiryPaging.kt
+++ src/main/kotlin/com/example/echo/domain/inquiry/repository/InquiryPaging.kt
@@ -4,7 +4,9 @@ import com.example.echo.domain.inquiry.dto.response.InquiryResponse
 import com.example.echo.domain.inquiry.entity.Inquiry
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
 
 interface InquiryPaging {
+    @Query("SELECT i FROM Inquiry i JOIN FETCH i.member WHERE i.member.memberId = :memberId")
     fun findAllInquiriesUser(memberId: Long, pageable: Pageable): Page<Inquiry>
 }
