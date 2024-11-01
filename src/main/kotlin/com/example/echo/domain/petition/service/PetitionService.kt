8f04c2ac2c5d11294e3d6dd347c5bde6b738c7a6
geun <minkeon44@naver.com>
2024-11-01 10:13:42 +0900

    청원 커밋

--- src/main/kotlin/com/example/echo/domain/petition/service/PetitionService.kt
+++ src/main/kotlin/com/example/echo/domain/petition/service/PetitionService.kt
@@ -15,30 +15,29 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
-import java.util.function.Function
 import java.util.stream.Collectors
 
 @Service
 class PetitionService (
     private val petitionRepository: PetitionRepository,
-    private val memberRepository: MemberRepository
-    // private val summarizationService: SummarizationService
+    private val memberRepository: MemberRepository,
+    private val SummarizationService: SummarizationService
 ){
     // 청원 등록
     @Transactional
     fun createPetition(petitionDto: PetitionRequestDto): PetitionDetailResponseDto {
         // 청원 등록을 위한 관리자 아이디 검색
-        val member = memberRepository!!.findById(petitionDto.memberId)
+        val member = memberRepository.findById(petitionDto.memberId)
             .orElseThrow { PetitionCustomException(ErrorCode.MEMBER_NOT_FOUND) }
 
         val petition = petitionDto.toEntity(member)
-        return PetitionDetailResponseDto(petitionRepository!!.save(petition))
+        return PetitionDetailResponseDto(petitionRepository.save(petition))
     }
 
     // 청원 단건 조회
     @Transactional // 요약 저장하는 경우 있음
     fun getPetitionById(petitionId: Long): PetitionDetailResponseDto {
-        val petition = petitionRepository!!.findById(petitionId).orElseThrow {
+        val petition = petitionRepository.findById(petitionId).orElseThrow {
             PetitionCustomException(
                 ErrorCode.PETITION_NOT_FOUND
             )
@@ -59,7 +58,7 @@ class PetitionService (
             val content = petition.content // 원본 내용
             // 줄바꿈 문단 처리 방식 -> 공백 전부 제거 or 줄바꿈은 유지
             val originText = content.replace("\\s+".toRegex(), " ")
-            val summaryText: String = summarizationService.getSummarizedText(originText) // 요약 결과
+            val summaryText: String = SummarizationService.getSummarizedText(originText)// 요약 결과
             // null -> 요약된 내용으로 변경
             petition.changeSummary(summaryText)
             petitionRepository.save(petition)
@@ -69,18 +68,18 @@ class PetitionService (
 
     // 청원 전체 조회
     fun getPetitions(pageable: Pageable): Page<PetitionResponseDto> {
-        return petitionRepository!!.findAll(pageable).map { PetitionResponseDto(it) }
+        return petitionRepository.findAll(pageable).map { PetitionResponseDto(it) }
     }
 
 
     // 진행 중인 청원 전체 조회
     fun getOngoingPetitions(pageable: Pageable): Page<PetitionResponseDto> {
-        return petitionRepository!!.findAllOngoing(pageable).map { PetitionResponseDto(it)}
+        return petitionRepository.findAllOngoing(pageable).map { PetitionResponseDto(it)}
     }
 
     // 청원 전체 조회 (카테고리별)
     fun getPetitionsByCategory(pageable: Pageable, category: Category): Page<PetitionResponseDto> {
-        return petitionRepository!!.findByCategory(pageable, category)
+        return petitionRepository.findByCategory(pageable, category)
             .map { PetitionResponseDto(it) }
     }
 
@@ -88,14 +87,14 @@ class PetitionService (
         // 청원 만료일 순 5개 조회
         get() {
             val pageable: Pageable = PageRequest.of(0, 5)
-            return petitionRepository!!.getEndDatePetitions(pageable)
+            return petitionRepository.getEndDatePetitions(pageable)
         }
 
     val likesCountPetitions: List<PetitionResponseDto>
         // 청원 동의자 순 5개 조회
         get() {
             val pageable: Pageable = PageRequest.of(0, 5)
-            return petitionRepository!!.getLikesCountPetitions(pageable)
+            return petitionRepository.getLikesCountPetitions(pageable)
         }
 
     // 좋아요 기능
@@ -106,12 +105,12 @@ class PetitionService (
         }
 
         // 멤버 존재 여부 확인
-        if (!memberRepository!!.existsById(memberId)) {
+        if (!memberRepository.existsById(memberId)) {
             throw PetitionCustomException(ErrorCode.MEMBER_NOT_FOUND) // 여기에서 발생시키는 예외
         }
 
         // 청원 조회
-        val petition = petitionRepository!!.findById(petitionId)
+        val petition = petitionRepository.findById(petitionId)
             .orElseThrow { PetitionCustomException(ErrorCode.PETITION_NOT_FOUND) }
 
         // 좋아요를 추가하거나 제거
@@ -128,16 +127,16 @@ class PetitionService (
     // 청원 카테고리 선택 5개 조회 (랜덤 순)
     fun getRandomCategoryPetitions(category: Category): List<PetitionResponseDto> {
         val pageable: Pageable = PageRequest.of(0, 5)
-        return petitionRepository!!.getCategoryPetitionsInRandomOrder(category, pageable)
+        return petitionRepository.getCategoryPetitionsInRandomOrder(category, pageable)
     }
 
     // 청원 수정
     @Transactional
     fun updatePetition(petitionId: Long, updatedPetitionDto: PetitionRequestDto): PetitionDetailResponseDto {
-        val existingPetition = petitionRepository!!.findById(petitionId)
+        val existingPetition = petitionRepository.findById(petitionId)
             .orElseThrow { PetitionCustomException(ErrorCode.PETITION_NOT_FOUND) }
 
-        val member = memberRepository!!.findById(updatedPetitionDto.memberId)
+        val member = memberRepository.findById(updatedPetitionDto.memberId)
             .orElseThrow { PetitionCustomException(ErrorCode.MEMBER_NOT_FOUND) }
 
         val updatedPetition = updatedPetitionDto.toEntityWithExistingData(existingPetition, member)
@@ -147,7 +146,7 @@ class PetitionService (
     // 청원 삭제
     @Transactional
     fun deletePetitionById(petitionId: Long) {
-        if (!petitionRepository!!.existsById(petitionId)) {
+        if (!petitionRepository.existsById(petitionId)) {
             throw PetitionCustomException(ErrorCode.PETITION_NOT_FOUND)
         }
         petitionRepository.deleteById(petitionId)
@@ -164,8 +163,12 @@ class PetitionService (
         return petition.endDate.plusDays(1).isBefore(LocalDateTime.now()) // 만료일이 지난 경우 true
     }
 
-    fun searchPetitionsByTitle(query: String): List<PetitionDetailResponseDto> {
-
+    // 제목으로 청원 검색
+    fun searchPetitionsByTitle(query: String?): List<PetitionDetailResponseDto> {
+        val petitions = petitionRepository.findByTitleContainingIgnoreCase(query!!)
+        return petitions.stream()
+            .map { petition: Petition? -> PetitionDetailResponseDto(petition!!) }
+            .collect(Collectors.toList())
     }
 
 }
