58f15c3b46807ebbcfeb642ebae81b0753d172d9
username0w <grandcias123@gmail.com>
2024-11-04 14:35:55 +0900

    feat: 상위 5개 반환을 위한 Pageable 추가

--- src/main/kotlin/com/example/echo/domain/petition/repository/AgeGroupInterestCountRepository.kt
+++ src/main/kotlin/com/example/echo/domain/petition/repository/AgeGroupInterestCountRepository.kt
@@ -1,6 +1,7 @@
 package com.example.echo.domain.petition.repository
 
 import com.example.echo.domain.petition.entity.AgeGroupInterestCount
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository
 interface AgeGroupInterestCountRepository : JpaRepository<AgeGroupInterestCount, Long> {
 
     @Query("SELECT a FROM AgeGroupInterestCount a WHERE a.ageGroup = :ageGroup ORDER BY a.interestCount DESC")
-    fun findTopPetitionsByAgeGroup(ageGroup: String): List<AgeGroupInterestCount>
+    fun findTopPetitionsByAgeGroup(ageGroup: String, pageable: Pageable): List<AgeGroupInterestCount>
 
     @Query("SELECT a FROM AgeGroupInterestCount a WHERE a.ageGroup = :ageGroup AND a.petition.petitionId = :petitionId")
     fun findByAgeGroupAndPetitionId(ageGroup: String, petitionId: Long): AgeGroupInterestCount?
