f0a8e44881730eb17d602d5201788c7dbe267b65
username0w <grandcias123@gmail.com>
2024-11-04 14:38:19 +0900

    feat: @Column, @JsonIgnore 추가

--- src/main/kotlin/com/example/echo/domain/petition/entity/AgeGroupInterestCount.kt
+++ src/main/kotlin/com/example/echo/domain/petition/entity/AgeGroupInterestCount.kt
@@ -1,5 +1,6 @@
 package com.example.echo.domain.petition.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.*
 
 @Entity
@@ -7,13 +8,17 @@ import jakarta.persistence.*
 class AgeGroupInterestCount (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interest_id")
     var id: Long? = null,
 
+    @Column(name = "age_group")
     val ageGroup: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "petition_id", nullable = false)
+    @JsonIgnore
     val petition: Petition? = null,
 
+    @Column(name = "interest_count")
     var interestCount: Int = 0
 )
