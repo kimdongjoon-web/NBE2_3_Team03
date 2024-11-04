dff6e615009af5a582ca374f78ec95b82b446f16
username0w <grandcias123@gmail.com>
2024-11-04 14:37:16 +0900

    feat: 순환참조 해결을 위한 PetitionDTO 추가

--- src/main/kotlin/com/example/echo/domain/petition/dto/response/AgeGroupInterestCountResponse.kt
+++ src/main/kotlin/com/example/echo/domain/petition/dto/response/AgeGroupInterestCountResponse.kt
@@ -8,14 +8,23 @@ import com.example.echo.global.exception.PetitionCustomException
 data class AgeGroupInterestCountResponse (
 
     val ageGroup: String,
-    val petition: Petition,
+    val petition: PetitionDTO,
     val interestCount: Int
 ) {
     companion object {
         fun fromEntity(ageGroupInterestCount: AgeGroupInterestCount): AgeGroupInterestCountResponse {
+            val petition = ageGroupInterestCount.petition ?: throw PetitionCustomException(ErrorCode.PETITION_NOT_FOUND)
+
             return AgeGroupInterestCountResponse(
                 ageGroup = ageGroupInterestCount.ageGroup,
-                petition = ageGroupInterestCount.petition ?: throw PetitionCustomException(ErrorCode.PETITION_NOT_FOUND),
+                petition = PetitionDTO(
+                    petitionId = petition.petitionId ?: throw PetitionCustomException(ErrorCode.PETITION_NOT_FOUND),
+                    title = petition.title!!,
+                    category = petition.category!!,
+                    likesCount = petition.likesCount,
+                    interestCount = petition.interestCount,
+                    agreeCount = petition.agreeCount!!
+                ),
                 interestCount = ageGroupInterestCount.interestCount
             )
         }
